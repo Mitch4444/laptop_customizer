47c5e4059a9c2942383519d0d07398442d01f538
Mitch4444 <eye8adog@gmail.com>
2019-04-10 14:22:22 -0700

    Update Total.js

--- src/Components/Total.js
+++ src/Components/Total.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './Total.css';
+import React from 'react'
 
-function Total(props) {
-  return (
-    <div className="summary__total__value">
-      {new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      }).format(props.total)}
-    </div>
-  );
+class Total extends React.Component{
+  render(){
+    return(
+     
+              <div className="summary__total">
+               <div className="summary__total__label">Your Price: </div>
+              <div className="summary__total__value">
+              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                  .format(this.props.total) }
+              </div>
+            </div>
+    )
+  }
 }
 
-export default Total;
+export default Total
